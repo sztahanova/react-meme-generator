ffdda65997e006de98fb05a5a0d0ae8d48cd88eb
sztahanova <sztahanova@github.com>
2021-02-09 22:19:48 +0100

    Fixed image links

--- src/links.js
+++ src/links.js
@@ -1,3 +1,3 @@
-export const TROLL_FACE_IMAGE = "http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png";
-export const DEFAULT_MEME_IMAGE = "http://i.imgflip.com/1bij.jpg";
+export const TROLL_FACE_IMAGE = "https://www.pikpng.com/pngl/b/47-476368_trollface-transparent-background-for-free-download-transparent-background.png";
+export const DEFAULT_MEME_IMAGE = "https://i.imgflip.com/1bij.jpg";
 export const MEME_IMAGES = "https://api.imgflip.com/get_memes";
